d9599bfed28247b4034f060a96e6319ce692666d
cvillegas5 <cvillegas5@miners.utep.edu>
2023-11-08 17:01:37 -0700

    black screen w/ line

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -6,6 +6,16 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
+f1()
+{
+  for (int i = 0; i <30; i++)
+    drawPixel(i,i,COLOR_PINK);
+}
+
+f2(){
+  for(int i =0; i<30; i++)
+    drawPixel(i,30-i, COLOR_PINK);
+}
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int
 main()
@@ -14,10 +24,13 @@ main()
   lcd_init();
   u_char width = screenWidth, height = screenHeight;
 
-  clearScreen(COLOR_BLUE);
+  clearScreen(COLOR_BLACK);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  // drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
 
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
-  
+  // fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+
+  f1();
+  f2();
+ 
 }
